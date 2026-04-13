0e1b04df8126cc67d8456f8e3dc8f05cb4d55219
Akumu9K <kaantaylan09@gmail.com>
2026-04-13 15:22:54 +0300

    Tiny comment change

--- Geodesic/customsyntax.lua
+++ Geodesic/customsyntax.lua
@@ -1,5 +1,5 @@
 -- Custom Syntax:
 
-custom_syntax = { -- The custom syntax uses luas pattern matching characters, so you can do stuff like "(any text here)"" to be a specific thing
+custom_syntax = { -- The custom syntax uses luas pattern matching characters, so you can do stuff like "(any text here)" to be a specific thing
     ["EXAMPLE CUSTOM SYNTAX"] = {ishexpattern = false},
 }
